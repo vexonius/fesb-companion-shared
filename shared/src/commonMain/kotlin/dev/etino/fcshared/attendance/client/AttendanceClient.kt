b38b5c2166b9e47a0d52ba2d3e4f3a4cb94a7102
vexonius <tino@etino.dev>
2025-03-06 19:14:01 +0100

    Deleted attendance feature files

--- shared/src/commonMain/kotlin/dev/etino/fcshared/attendance/client/AttendanceClient.kt
+++
@@ -1,9 +0,0 @@
-package dev.etino.fcshared.attendance.client
-
-interface AttendanceClient {
-
-    suspend fun getAttendanceItems(): String
-
-    suspend fun getAttendanceItem(resourcePath: String): String
-
-}
--- shared/src/commonMain/kotlin/dev/etino/fcshared/attendance/models/AttendanceTableItemResponse.kt
+++
@@ -1,3 +0,0 @@
-package dev.etino.fcshared.attendance.models
-
-data class AttendanceTableItemResponse(val `class`: String, val partialUrl: String, val semester: Int)
