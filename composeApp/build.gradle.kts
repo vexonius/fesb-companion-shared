1ceccf17d61e28b84c9ea69994406dec8f976dca
stipe-jurkovic <sjurko00@fesb.hr>
2026-05-28 08:56:24 +0200

    possible fix 2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -21,7 +21,6 @@ kotlin {
         }
     }
     listOf(
-        iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
@@ -94,8 +93,6 @@ dependencies {
     // iOS
 
     add("kspIosSimulatorArm64", libs.androidx.room.compiler)
-
-    add("kspIosX64", libs.androidx.room.compiler)
 
     add("kspIosArm64", libs.androidx.room.compiler)
 
