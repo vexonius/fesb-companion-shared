032e43692b9f7361d99f09ab28f304ac649732a3
stipe-jurkovic <sjurko00@fesb.hr>
2026-05-31 18:46:13 +0200

    moved the koin

--- composeApp/src/iosMain/kotlin/dev/etino/fcshared/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dev/etino/fcshared/MainViewController.kt
@@ -1,37 +1,10 @@
 package dev.etino.fcshared
 
-import platform.UIKit.UIViewController
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.etino.fcshared.application.ApplicationWrapper
-import dev.etino.fcshared.features.attendance.di.attendanceModule
-import dev.etino.fcshared.features.home.di.homeModule
-import dev.etino.fcshared.features.iksica.di.iksicaModule
-import dev.etino.fcshared.features.login.di.loginModule
-import dev.etino.fcshared.features.menza.di.menzaModule
-import dev.etino.fcshared.features.settings.di.settingsModule
-import dev.etino.fcshared.features.studomat.di.studomatModule
-import dev.etino.fcshared.features.timetable.di.timetableModule
-import dev.etino.fcshared.networking.servicesModule
-import org.koin.core.context.startKoin
+import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController {
-
-    startKoin {
-        modules(
-            attendanceModule,
-            loginModule,
-            dbModule,
-            iosNetworkModule,
-            servicesModule,
-            timetableModule,
-            homeModule,
-            settingsModule,
-            menzaModule,
-            iksicaModule,
-            studomatModule,
-        )
-    }
-
     return ComposeUIViewController {
         ApplicationWrapper()
     }
